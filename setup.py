c381910c061abb56d242603933c88bbe53305912
Vincent Caldeira <vincent.caldeira@gmail.com>
2023-08-06 19:23:33 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experimentation with Foundation Models for demo purpose',
+    author='Vincent Caldeira',
+    license='MIT',
+)
